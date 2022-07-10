2d27a8c5278b66d7b4613b7d324e870c319c32fd
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-07-10 17:44:20 +0200

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="contents">
+    <textarea id="json" v-model="json"></textarea>
+  </div>
+  <div class="content">
+    <Codemirror
+      v-model:value="code"
+      :options="cmOptions"
+      border
+      placeholder="test placeholder"
+      :height="575"
+      :width="620"
+      @change="change"
+    />
+    <pre class="code">{{ json }}</pre>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Codemirror from "codemirror-editor-vue3";
 
+// language
+import "codemirror/mode/javascript/javascript.js";
+
+// theme
+import "codemirror/theme/dracula.css";
+
+import { ref } from "vue";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  components: { Codemirror },
+  setup() {
+    const json = ref("Enter Json Here");
+    const code = ref(`
+var i = 0;
+for (; i < 9; i++) {
+  console.log(i);
+  // more statements
+}`);
+
+    return {
+      code,
+      json,
+      cmOptions: {
+        mode: "text/javascript", // Language mode
+        theme: "dracula", // Theme
+        lineNumbers: true, // Show line number
+        smartIndent: true, // Smart indent
+        indentUnit: 2, // The smart indent unit is 2 spaces in length
+        foldGutter: true, // Code folding
+        styleActiveLine: true, // Display the style of the selected row
+      },
+    };
   },
 };
 </script>
+<style>
+.content {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+#json {
+  width: 30vw;
+  height: 20vh;
+  margin: 0 1rem;
+  border: 1px solid #ddd;
+}
+/* center .contents div childs */
+.contents {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 20vh;
+}
+#contents {
+  margin-top: 5px;
+}
+.code,
+.codemirror .cm-editor {
+  width: 30vw;
+  height: 50vh;
+  margin: 0 1rem;
+  border: 1px solid #ddd;
+  overflow: scroll;
+}
+.CodeMirror {
+  font-family: Arial, monospace;
+  font-size: 16px;
+}
+</style>
